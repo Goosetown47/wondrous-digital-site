aa3e2dc74f4212c8116f1d57563467e4ef3bc38b
Tyler LaHaie <147102475+Goosetown47@users.noreply.github.com>
2025-09-28 16:34:37 -0400

    Update component registry

--- nextjs-app/src/lib/register-components-generated.tsx
+++ nextjs-app/src/lib/register-components-generated.tsx
@@ -1,12 +1,13 @@
 // THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
-// Generated at: 2025-09-28T19:34:58.157Z
-// Components: 3
+// Generated at: 2025-09-28T20:34:36.805Z
+// Components: 4
 
 import { ComponentRegistry } from '@/lib/component-registry';
 
 import { Hero1, hero1Config } from '@/components/core/sections/hero1';
 import { Hero2, hero2Config } from '@/components/core/sections/hero2';
 import { Hero3, hero3Config } from '@/components/core/sections/hero3';
+import { Hero4, hero4Config } from '@/components/core/sections/hero4';
 
 export function registerGeneratedComponents() {
   ComponentRegistry.register('Hero1', {
@@ -31,5 +32,13 @@ export function registerGeneratedComponents() {
     defaultContent: hero3Config.defaultContent,
     editableFields: hero3Config.editableFields,
     source: 'expansions'
+  });
+
+  ComponentRegistry.register('Hero4', {
+    component: Hero4,
+    type: 'section',
+    defaultContent: hero4Config.defaultContent,
+    editableFields: hero4Config.editableFields,
+    source: 'shadcn'
   });
 }
